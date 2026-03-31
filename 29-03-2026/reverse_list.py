35c8653001de91917b40b361680440cd3345525c
Madhumitha212 <madhuravi324@gmail.com>
2026-03-31 10:32:48 +0530

    add: [Madhu]-added logic to reverse the list and string

---
+++ 29-03-2026/reverse_list.py
@@ -0,0 +1,10 @@
+# List
+nums = [1, 2, 3, 4]
+reversed_list = nums[::-1]
+
+# String
+text = "Python"
+reversed_text = text[::-1]
+
+print("Reversed list:", reversed_list)
+print("Reversed string:", reversed_text)
